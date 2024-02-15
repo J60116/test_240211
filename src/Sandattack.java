28078040bb7a322beb2dda5b58edcbb8d759d183
mow <j16.gleen8@gmail.com>
2024-02-15 23:30:46 +0900

    すなかけの最大MPを訂正

--- src/Sandattack.java
+++ src/Sandattack.java
@@ -4,7 +4,7 @@ public class Sandattack extends Move {
         super();
         this.setName("Sand attack");
         this.setType(Pokemon.ARRAY_TYPE[0]);//NOrmal
-        this.setMP_max(40);
+        this.setMP_max(15);
         this.setMP(this.getMP_max());
         this.setMoveType(ARRAY_MOVE_TYPE[2]);
         this.setPower(0);
