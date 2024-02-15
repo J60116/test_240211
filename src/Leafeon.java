d79d056d563077053be51ccd3171ad68aa0cf5d4
mow <j16.gleen8@gmail.com>
2024-02-15 21:01:04 +0900

    要素番号の修正

--- src/Leafeon.java
+++ src/Leafeon.java
@@ -8,7 +8,7 @@ final class Leafon extends Eevee{
 
 	public Leafon(String owner, String ball){ 
 		super(owner, ball);
-		super.setEvolvedConstructor(0);
+		super.setEvolvedConstructor(3);
 		this.setHP(this.getHP_max());
 		this.setMoves(1, new Leafblade());
 	}
