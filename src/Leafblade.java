c0c69e040d55b6a27fdf6a5a72ca49da5bfd7037
mow <j16.gleen8@gmail.com>
2024-02-15 21:07:14 +0900

    Leafbladeクラスの修正

--- src/Leafblade.java
+++ src/Leafblade.java
@@ -4,9 +4,9 @@ public class Leafblade extends Move {
         super();
         this.setName("Leafblade");
         this.setType(Pokemon.ARRAY_TYPE[4]);//Grass
-        this.setMP_max(35);
+        this.setMP_max(15);
         this.setMP(this.getMP_max());
-        this.setPower(10);
+        this.setPower(90);
         this.setAccuracy(100);
     }
     
