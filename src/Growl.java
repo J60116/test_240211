b76bd54c0c4d30674be9dfd2381a5aa8fd08ef27
mow <j16.gleen8@gmail.com>
2024-02-15 21:16:44 +0900

    なきごえの追加

---
+++ src/Growl.java
@@ -0,0 +1,13 @@
+public class Growl extends Move {
+
+    public Growl(){
+        super();
+        this.setName("Growl");
+        this.setType(Pokemon.ARRAY_TYPE[0]);//NOrmal
+        this.setMP_max(40);
+        this.setMP(this.getMP_max());
+        this.setPower(0);
+        this.setAccuracy(100);
+    }
+    
+}
