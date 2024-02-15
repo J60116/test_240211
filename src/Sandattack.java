7c6823f0171995e531241455adf4c8aabb535fa8
mow <j16.gleen8@gmail.com>
2024-02-15 21:59:15 +0900

    すなかけの技名の修正

--- src/Sandattack.java
+++ src/Sandattack.java
@@ -2,7 +2,7 @@ public class Sandattack extends Move {
 
     public Sandattack(){
         super();
-        this.setName("Growl");
+        this.setName("Sand attack");
         this.setType(Pokemon.ARRAY_TYPE[0]);//NOrmal
         this.setMP_max(40);
         this.setMP(this.getMP_max());
