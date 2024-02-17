06bb72ef691fe9633f00d6c681bbc3eff165fbc6
mow <j16.gleen8@gmail.com>
2024-02-17 16:38:21 +0900

    JolteonとFlareonのインスタンスを生成、高台と展望台に行く

--- src/game/main/Main.java
+++ src/game/main/Main.java
@@ -13,17 +13,18 @@ public class Main {
 		Eevee eevee = new Eevee();
 		Leafeon leafeon = new Leafeon();
 		Vaporeon vaporeon = new Vaporeon();
-		Jolteon j = new Jolteon();
-
+		Jolteon jolteon = new Jolteon();
+		Flareon flareon = new Flareon();
+		
 		//所持しているポケモンの確認
 		user.viewCurrentParty();
 		
 		//冒険に出かける
-		user.goTo("hill", j);
 		user.goTo("flower garden", eevee);
-		user.goTo("grassland", eevee);
 		user.goTo("grassland", leafeon);
 		user.goTo("river", vaporeon);
+		user.goTo("hill", jolteon);
+		user.goTo("observatory", flareon);
 
 		//所持しているポケモンの確認
 		user.viewCurrentParty();
